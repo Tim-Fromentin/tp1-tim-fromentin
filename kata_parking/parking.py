8db4de9c2a933ef25e78a91a58ec4b026185b3b4
Tim <tim.fromentin.dev.pro@gmail.com>
2026-09-16 11:54:33 +0200

    green: au-delà de 72 heures, le véhicule est déclaré en fourrière et le montant est un forfait de 250 euros

--- kata_parking/parking.py
+++ kata_parking/parking.py
@@ -1,6 +1,8 @@
 import math
 
 def calculer_prix_parking(duree_minute, vehicule="voiture", heure_entree=None, heure_sortie=None):
+    if duree_minute > 4320:
+        return 250.00
     if heure_entree and heure_sortie and heure_entree > heure_sortie:
         return "Attention, une erreur s'est produite concernant votre heure de sortie."
     min_gratuite = 30
