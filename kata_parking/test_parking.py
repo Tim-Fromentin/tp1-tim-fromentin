dea2b9f3032790d897d2c5835f9a5ff93c18cbfe
Tim <tim.fromentin.dev.pro@gmail.com>
2026-09-16 10:48:03 +0200

    red:Le montant est plafonné à 18 euros par tranche de 24 heures commencée

--- kata_parking/test_parking.py
+++ kata_parking/test_parking.py
@@ -9,3 +9,8 @@ def test_tout_demi_heure_facturee_1_50():
     duree_minute = 31
     prix = calculer_prix_parking(duree_minute)
     assert prix == 1.50
+
+def test_maximum_18_euro_par_tranche_24h():
+    duree_minute = 480
+    prix = calculer_prix_parking(duree_minute)
+    assert prix == 18
