d30ef90eb7d7388e19b143d0e199d8b741830cf6
Tim <tim.fromentin.dev.pro@gmail.com>
2026-09-16 13:37:00 +0200

    test: valeur du stock est la somme des quantités multipliées par les prix unitaires hors taxe, arrondie au centime

---
+++ inventaire/test_inventaire_legacy.py
@@ -0,0 +1,10 @@
+from inventaire import val
+
+ARTICLES = [
+    {"ref": "VIS-M6", "lib": "Vis M6 acier", "q": 2, "pu": 0.15, "seuil": 20, "cat": "piece"},
+    {"ref": "PERC-18", "lib": "Perceuse 18V", "q": 10, "pu": 1, "seuil": 3, "cat": "outil"},
+]
+
+def test_val():
+    resultat = val(ARTICLES)
+    assert resultat == 10.30
