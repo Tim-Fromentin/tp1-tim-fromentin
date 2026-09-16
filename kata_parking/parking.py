671ec9962ad50d122d176f06504eba4bc1841f78
Tim <tim.fromentin.dev.pro@gmail.com>
2026-09-16 10:40:17 +0200

    green: Au-delà de 30 minutes, chaque demi-heure commencée est facturée 1,50 euro

--- kata_parking/parking.py
+++ kata_parking/parking.py
@@ -1,2 +1,8 @@
+import math
+
 def calculer_prix_parking(duree_minute):
-    return 0.0
+    if duree_minute <= 30:
+        return 0.0
+    if duree_minute > 30:
+        tranche = duree_minute / 30
+        return math.floor(tranche) * 1.5
